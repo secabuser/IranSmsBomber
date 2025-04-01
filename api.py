da74ad841537099cd0b4d0474c0b5341ba863b6d
r.gh <hiradsec@gmail.com>
2025-04-01 22:20:41 +0330

    Update api.py

--- api.py
+++ api.py
@@ -76,9 +76,10 @@ SERVICES = {
     'ghabzino': lambda num, proxies: post(
         proxies=proxies,
         url='https://application2.billingsystem.ayantech.ir/WebServices/Core.svc/requestActivationCode',
-        json={"Parameters": {"ApplicationType": "Web","ApplicationUniqueToken": None,"ApplicationVersion": "1.0.0","MobileNumber": f'0{num}'}}
+        json={"Parameters": {"ApplicationType": "Web","ApplicationUniqueToken": None,"ApplicationVersion": "1.0.0","MobileNumber": f'0{num}'}},
         headers={'accept': 'application/json, text/plain, */*','accept-encoding': 'gzip, deflate, br','accept-language': 'en-US,en;q=0.9','content-type': 'application/json','origin': 'https://ghabzino.com','referer': 'https://ghabzino.com/','user-agent': generate_user_agent(os="linux")}
     ),
+
     
     'miareh': lambda num, proxies: get(
         proxies=proxies,
@@ -86,10 +87,10 @@ SERVICES = {
     ),
     
     'divar': lambda num, proxies: post(
-        proxies=proxies,
-        url="https://api.divar.ir/v5/auth/authenticate",
-        json={"phone":f"0+{num}"}
-        headers = {
+    proxies=proxies,
+    url="https://api.divar.ir/v5/auth/authenticate",
+    json={"phone": f"0{num}"},
+    headers={
         "Accept": "application/json, text/plain, */*",
         "Accept-Encoding": "gzip, deflate, br, zstd",
         "Accept-Language": "en-US,en;q=0.7",
@@ -109,7 +110,8 @@ SERVICES = {
         "User-Agent": generate_user_agent(os="linux"),
         "X-Standard-Divar-Error": "true"
     }
-    ),
+),
+
     
     'drnext': lambda num, proxies: post(
         proxies=proxies,
@@ -127,15 +129,15 @@ SERVICES = {
     ),
     
     
-    'doctoreto': lambda num, proxies: get(
-        proxies=proxies,
-        url="https://api.doctoreto.com/api/web/patient/v1/accounts/register",
-        json={
-            "mobile": num,
-            "captcha": "",
-            "country_id": 205
-        }
-        headers={
+    'doctoreto': lambda num, proxies: post(
+    proxies=proxies,
+    url="https://api.doctoreto.com/api/web/patient/v1/accounts/register",
+    json={
+        "mobile": num,
+        "captcha": "",
+        "country_id": 205
+    },
+    headers={
         "Accept": "application/json",
         "Accept-Encoding": "gzip, deflate, br, zstd",
         "Accept-Language": "en-US,en;q=0.9",
@@ -143,7 +145,7 @@ SERVICES = {
         "Connection": "keep-alive",
         "Content-Length": "53",
         "Content-Type": "application/json",
-        "Cookie": "UG=nsECNSsSWE81; SUG=cIn9JugaP45; _gcl_au=1.1.253851887.1732379376; _gid=GA1.2.810861762.1732379379; _gat_UA-81198296-1=1; _clck=ha9b9p%7C2%7Cfr4%7C0%7C1788; _ga=GA1.2.1300353040.1732379378; _clsk=j3jl3t%7C1732379393879%7C3%7C0%7Cw.clarity.ms%2Fcollect; _ga_YYV44WWYMV=GS1.1.1732379378.1.1.1732379394.44.0.0",
+        "Cookie": "UG=nsECNSsSWE81; SUG=cIn9JugaP45; ...",  # ادامه کوکی
         "Host": "api.doctoreto.com",
         "Origin": "https://doctoreto.com",
         "Referer": "https://doctoreto.com/",
@@ -154,8 +156,9 @@ SERVICES = {
         "Sec-Fetch-Mode": "cors",
         "Sec-Fetch-Site": "same-site",
         "User-Agent": generate_user_agent(os="mac")
-        }
+    }
     ),
+
     
     'banimod': lambda num, proxies: post(
         proxies=proxies,
@@ -195,39 +198,40 @@ SERVICES = {
         proxies=proxies,
         url="https://www.hamrah-mechanic.com/api/v1/membership/otp",
         json={
-            "PhoneNumber": f'0{num}',
-            "prevDomainUrl": "https://www.google.com/",
-            "landingPageUrl": "https://www.hamrah-mechanic.com/cars-for-sale/",
-            "orderPageUrl": "https://www.hamrah-mechanic.com/membersignin/",
-            "prevUrl": "https://www.hamrah-mechanic.com/cars-for-sale/",
-            "referrer": "https://www.google.com/"
-        }
-        headers = {
-    "_uti": "3b534c14-3afd-4907-9cfe-258fc84978eb",
-    "Accept": "application/json",
-    "Accept-Encoding": "gzip, deflate, br, zstd",
-    "Accept-Language": "en-US,en;q=0.9",
-    "Connection": "keep-alive",
-    "Content-Length": "272",
-    "Content-Type": "application/json",
-    "Cookie": "show-login=showed; EndPage=1; _uti=3b534c14-3afd-4907-9cfe-258fc84978eb; analytics_campaign={%22source%22:%22bing%22%2C%22medium%22:%22organic%22}; analytics_token=f63d7938-b6d3-1d0b-5ba2-490f07963a54; analytics_session_token=20c6c793-c10d-0917-d523-6f19dc4768b1; yektanet_session_last_activity=11/23/2024; _yngt_iframe=1; _gcl_au=1.1.730629661.1732378841; _yngt=01JDCX6KM6DV2XBWEP7ZJE4SRA; _ga=GA1.2.2036949947.1732378842; _gid=GA1.2.90801843.1732378842; _gat_UA-106934660-1=1; _hjSessionU...",
-    "env": "prd",
-    "fcm": "",
-    "gps_ad_id": "",
-    "Host": "www.hamrah-mechanic.com",
-    "install_source": "",
-    "Origin": "https://www.hamrah-mechanic.com",
-    "Referer": "https://www.hamrah-mechanic.com/membersignin/",
-    "sec-ch-ua": "\"Microsoft Edge\";v=\"131\", \"Chromium\";v=\"131\", \"Not_A Brand\";v=\"24\"",
-    "sec-ch-ua-mobile": "?0",
-    "sec-ch-ua-platform": "\"Windows\"",
-    "Sec-Fetch-Dest": "empty",
-    "Sec-Fetch-Mode": "cors",
-    "Sec-Fetch-Site": "same-origin",
-    "Source": "web",
-    "User-Agent": generate_user_agent(os="linux")
-}
-    ),
+          "PhoneNumber": f'0{num}',
+          "prevDomainUrl": "https://www.google.com/",
+          "landingPageUrl": "https://www.hamrah-mechanic.com/cars-for-sale/",
+          "orderPageUrl": "https://www.hamrah-mechanic.com/membersignin/",
+          "prevUrl": "https://www.hamrah-mechanic.com/cars-for-sale/",
+          "referrer": "https://www.google.com/"
+        },
+        headers={
+           "_uti": "3b534c14-3afd-4907-9cfe-258fc84978eb",
+           "Accept": "application/json",
+            "Accept-Encoding": "gzip, deflate, br, zstd",
+           "Accept-Language": "en-US,en;q=0.9",
+            "Connection": "keep-alive",
+            "Content-Length": "272",
+           "Content-Type": "application/json",
+           "Cookie": "show-login=showed; EndPage=1; ...",
+           "env": "prd",
+           "fcm": "",
+           "gps_ad_id": "",
+           "Host": "www.hamrah-mechanic.com",
+           "install_source": "",
+           "Origin": "https://www.hamrah-mechanic.com",
+          "Referer": "https://www.hamrah-mechanic.com/membersignin/",
+           "sec-ch-ua": "\"Microsoft Edge\";v=\"131\", \"Chromium\";v=\"131\", \"Not_A Brand\";v=\"24\"",
+          "sec-ch-ua-mobile": "?0",
+          "sec-ch-ua-platform": "\"Windows\"",
+          "Sec-Fetch-Dest": "empty",
+          "Sec-Fetch-Mode": "cors",
+          "Sec-Fetch-Site": "same-origin",
+          "Source": "web",
+          "User-Agent": generate_user_agent(os="linux")
+    }
+),
+
     
     'lendo': lambda num, proxies: post(
         proxies=proxies,
@@ -392,7 +396,7 @@ SERVICES = {
         proxies=proxies,
         url="https://api.doctoreto.com/api/web/patient/v1/accounts/register",
         json={
-            "mobile": f"{number}",
+            "mobile": f"{num}",
             "country_id": 205,
             "captcha": ""
         }
